b37d3a57b5720f4fbc89fcd23bdf17adabc3cf86
lisa9000 <lisasalomons9000@gmail.com>
2020-02-10 17:49:52 +0100

    Added callibration from stored images

--- CameraCalibration/CameraCalibration/main.cpp
+++ CameraCalibration/CameraCalibration/main.cpp
@@ -49,12 +49,10 @@ void cameraCalibration(vector<Mat> calImages, Size boardSize, float edgeLen, Mat
 {
 	vector<vector<Point2f>> imgSpacePoints;
 	getCorners(calImages, imgSpacePoints, false);
-
 	vector<vector<Point3f>> worldCornerPoints(1); 
-
 	createKnowBoardPositions(boardSize, edgeLen, worldCornerPoints[0]);
 	worldCornerPoints.resize(imgSpacePoints.size(), worldCornerPoints[0]);
-
+	
 	vector<Mat> rVectors, tVectors; 
 	distCoef = Mat::zeros(8, 1, CV_64F); 
 
@@ -75,6 +73,7 @@ bool saveCamCalibration(string filename, Mat cameraMat, Mat distCoef)
 			for (int c = 0; c < cols; c++)
 			{
 				double value = cameraMat.at<double>(r, c);
+				cout << value << "\n";
 				outStream << value << endl; 
 			}
 		}
@@ -86,14 +85,17 @@ bool saveCamCalibration(string filename, Mat cameraMat, Mat distCoef)
 			for (int c = 0; c < cols; c++)
 			{
 				double value = distCoef.at<double>(r, c);
+				cout << value << "\n";
 				outStream << value << endl;
 			}
 		}
 
 		outStream.close();
+		cout << "Camera Calibration saved.\n";
 		return true;
 
 	}
+	cout << "Camera Calibration not saved.\n";
 	return false; 
 }
 
@@ -103,59 +105,81 @@ int main(int argv, char** argc)
 	Mat frame;
 	Mat drawToFrame;
 	Mat cameraMat = Mat::eye(3, 3, CV_64F);
-	Mat distanceCoefficients; 
-	vector<Mat> savedImages; 
+	Mat distanceCoefficients;
+	vector<Mat> savedImages;
 	vector<vector<Point2f>> markerCorners, rejectedCanidates;
-	
-	VideoCapture vid(0);
 
-	if (!vid.isOpened())
-		return -1;
+	cout << "Press i for images and v for live video, then press enter \n";
+	char ch = getchar();
 
-	int framePerSecond = 20; 
-	namedWindow("Webcam", WINDOW_AUTOSIZE);
-
-	while (true)
+	if (ch == 'i')
 	{
-		if (!vid.read(frame))
-			break; 
+		vector<cv::String> fn;
+		glob("C:/Users/Lisa/Pictures/Camera Roll/*.jpg", fn, false);
 
-		vector<Vec2f> foundPoints;
-		bool found = false;
-		found = findChessboardCorners(frame, boardDim, foundPoints, CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_FAST_CHECK | CALIB_CB_NORMALIZE_IMAGE);
-		frame.copyTo(drawToFrame);
-		drawChessboardCorners(drawToFrame, boardDim, foundPoints, found);
-		if (found)
-			imshow("Webcam", drawToFrame);
-		else
-			imshow("Webcam", frame);
+		size_t count = fn.size(); //number of png files in images folder
+		for (size_t i = 0; i < 1; i++)
+			savedImages.push_back(imread(fn[i]));
+		
+		cout << "images loaded \n";
+		cout << "Starting Camera Calibration. \n";
+		cameraCalibration(savedImages, boardDim, squareDim, cameraMat, distanceCoefficients);
+		cout << "Camera calibration complete. \n";
+		saveCamCalibration("CalibrationValues", cameraMat, distanceCoefficients);
+	}
 
-		char character = waitKey(1000 / framePerSecond);
+	if (ch == 'v')
+	{
+		VideoCapture vid(0);
 
-		switch (character)
+		if (!vid.isOpened())
+			return -1;
+
+		int framePerSecond = 20;
+		namedWindow("Webcam", WINDOW_AUTOSIZE);
+
+		while (true)
 		{
-		case ' ':// Spacebar
-			//save image for calibration
+			if (!vid.read(frame))
+				break;
+
+			vector<Vec2f> foundPoints;
+			bool found = false;
+			found = findChessboardCorners(frame, boardDim, foundPoints, CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_FAST_CHECK | CALIB_CB_NORMALIZE_IMAGE);
+			frame.copyTo(drawToFrame);
+			drawChessboardCorners(drawToFrame, boardDim, foundPoints, found);
 			if (found)
+				imshow("Webcam", drawToFrame);
+			else
+				imshow("Webcam", frame);
+
+			char character = waitKey(1000 / framePerSecond);
+
+			switch (character)
 			{
-				Mat temp;
-				frame.copyTo(temp);
-				savedImages.push_back(temp);
+			case ' ':// Spacebar
+				//save image for calibration
+				if (found)
+				{
+					Mat temp;
+					frame.copyTo(temp);
+					savedImages.push_back(temp);
+				}
+				break;
+			case 13:// Enter
+				//Start calibration
+				// If you saved 15 images at least with space you can start calibration
+				if (savedImages.size() > 15)
+				{
+					cameraCalibration(savedImages, boardDim, squareDim, cameraMat, distanceCoefficients);
+					saveCamCalibration("Calibration", cameraMat, distanceCoefficients);
+				}
+				break;
+			case 27: //Escape
+				// exit
+				return 0;
+				break;
 			}
-			break;
-		case 13:// Enter
-			//Start calibration
-			// If you saved 15 images at least with space you can start calibration
-			if (savedImages.size() > 15) 
-			{
-				cameraCalibration(savedImages, boardDim, squareDim, cameraMat, distanceCoefficients);
-				saveCamCalibration("Calibration", cameraMat, distanceCoefficients);
-			}
-			break;
-		case 27: //Escape
-			// exit
-			return 0; 
-			break;
 		}
 	}
 }
